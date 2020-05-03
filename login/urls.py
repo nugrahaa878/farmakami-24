619376a6257464c238a9e4042f75fca985808dc0
Ari Nugraha <nugrahaa878@gmail.com>
2020-05-03 04:07:01 -0700

    add form for admin cs kurir and konsumen

--- login/urls.py
+++ login/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('register/admin', views.adminForm, name="form-admin"),
+    path('register/cs', views.csForm, name="form-cs"),
+    path('register/konsumen', views.konsumenForm, name="form-konsumen"),
+    path('register/kurir', views.kurirForm, name="form-kurir"),
 ]
--- login/views.py
+++ login/views.py
@@ -8,3 +8,15 @@ def welcome(request):
 
 def register(request):
     return render(request, 'register.html')
+
+def adminForm(request):
+    return render(request, 'form/admin.html')
+
+def csForm(request):
+    return render(request, 'form/cs.html')
+
+def konsumenForm(request):
+    return render(request, 'form/konsumen.html')
+
+def kurirForm(request):
+    return render(request, 'form/kurir.html')
